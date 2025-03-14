f39347e439144bfb448fb3eb4ee9f3707f579f4e
sashishuu <emailkamu@gmail.com>
2025-03-14 19:24:26 +0700

    Refactored DataSeedService to use batch insert for student_courses soalnya lama

--- src/main/java/com/advpro/profiling/tutorial/service/DataSeedService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/DataSeedService.java
@@ -10,6 +10,7 @@ import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
@@ -28,11 +29,13 @@ public class DataSeedService {
     @Autowired
     private StudentCourseRepository studentCourseRepository;
 
-    private static final int NUMBER_OF_STUDENTS = 20_000;
-    private static final int NUMBER_OF_COURSE = 10;
+    private static final int NUMBER_OF_STUDENTS = 5_000;
+    private static final int NUMBER_OF_COURSE = 5;
+    private static final int MAX_COURSES_PER_STUDENT = 2;  // Reduce for efficiency
 
     public void seedStudent() {
         Faker faker = new Faker(new Locale("in-ID"));
+        List<Student> students = new ArrayList<>();
 
         for (int i = 0; i < NUMBER_OF_STUDENTS; i++) {
             Student student = new Student();
@@ -41,39 +44,48 @@ public class DataSeedService {
             student.setFaculty(faker.educator().course());
             student.setGpa(faker.number().randomDouble(2, 2, 4));
 
-            studentRepository.save(student);
+            students.add(student);
         }
+
+        studentRepository.saveAll(students);
     }
 
     public void seedCourse() {
         Faker faker = new Faker(new Locale("in-ID"));
+        List<Course> courses = new ArrayList<>();
+
         for (int i = 0; i < NUMBER_OF_COURSE; i++) {
             Course course = new Course();
             course.setCourseCode(faker.code().ean8());
             course.setName(faker.book().title());
             course.setDescription(faker.lorem().sentence());
 
-            courseRepository.save(course);
+            courses.add(course);
         }
+
+        courseRepository.saveAll(courses);
     }
 
     public void seedStudentCourses() {
         List<Student> students = studentRepository.findAll();
         List<Course> courses = courseRepository.findAll();
+        List<StudentCourse> studentCourses = new ArrayList<>();
+
+        Random random = new Random();
 
         for (Student student : students) {
-            List<Course> selectedCourses = new Random().ints(0, courses.size())
+            List<Course> selectedCourses = random.ints(0, courses.size())
                     .distinct()
-                    .limit(2)
+                    .limit(MAX_COURSES_PER_STUDENT)  // Reduce load on database
                     .mapToObj(courses::get)
                     .collect(Collectors.toList());
 
             for (Course course : selectedCourses) {
                 StudentCourse studentCourse = new StudentCourse(student, course);
-                studentCourseRepository.save(studentCourse);
+                studentCourses.add(studentCourse);
             }
         }
 
+        studentCourseRepository.saveAll(studentCourses);
     }
-
 }
